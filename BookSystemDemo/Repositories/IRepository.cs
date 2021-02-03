77700e8439a4f5a38099924e282367e41140d62b
patrick <patrick@makanda.io>
2021-02-03 19:38:48 +0100

    add generic repository interface

---
+++ BookSystemDemo/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookSystemDemo.Repositories
+{
+    public interface IRepository<T1, T2> where T1:class
+    {
+        Task<IEnumerable<T1>> GetAll();
+        Task<T1> GetById(T2 id);
+        Task<T1> Insert(T1 entity);
+        Task Delete(T2 id);
+        Task Save();
+    }
+}
